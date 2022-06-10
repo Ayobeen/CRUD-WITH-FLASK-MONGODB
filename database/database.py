6bc33e99248d96d714d2136f751b6959d7bb2089
Abdlazeez Ayobanji Mumeen <42062546+Ayobeen@users.noreply.github.com>
2022-06-10 07:27:18 +0100

    Update database.py

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 # Setup database connection
 try:
-    client = MongoClient("mongodb+srv://ayobanji:jinaboya1212@cluster0.uirbk.mongodb.net/?retryWrites=true&w=majority&authSource=admin")
+    client = MongoClient("mongodb+srv://ayobanji:password123@cluster0.uirbk.mongodb.net/?retryWrites=true&w=majority&authSource=admin")
     db = client["sloovi"]
     db.create_collection("users")
     db.create_collection("templates")
